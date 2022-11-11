ea8a7c0910cef580644767ed77b86fefca8f1be2
Micaela Rosadio <rosadio.karen@gmail.com>
2022-11-11 15:12:45 +0100

    Solved all tasks except Nr. 4

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/* -------------------------- */
+/* Import data and connect DB */
+/* -------------------------- */
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -21,3 +26,52 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+/* -------------------------- */
+/* 2. Create a recipe         */
+/* -------------------------- */
+
+Recipe.create({ title: "Bean Soup" , cuisine: "british" })
+      .then(createdRecipe => console.log(createdRecipe))
+      .catch(err => console.log(err))
+
+
+/* -------------------------- */
+/* 3. Insert multiple recipes */
+/* -------------------------- */
+
+Recipe.insertMany([
+  { title: "Creme broule", cuisine: "French" },
+  { title: "Pho soup"    , cuisine: "Vietnamese"}
+])
+.then(createdBooks => console.log(createdBooks))
+.catch(err => console.log(err));
+
+
+/* -------------------------- */
+/* 4. Update recipe           */
+/* -------------------------- */
+
+// Recipe.findOneAndUpdate({ title: "Creme broule" }, { author: "Third author" })
+//     .then(book => console.log(book))
+//     .catch(err => console.log(err));
+
+    /* Ask Marco */
+
+/* -------------------------- */
+/* 5. Remove recipe           */
+/* -------------------------- */
+
+Recipe.findOneAndDelete({ title: "Pho soup" })
+    .then(deletedRecipe => console.log(deletedRecipe))
+    .catch(err => console.log(err));
+
+
+/* -------------------------- */
+/* 6. Close the Database      */
+/* -------------------------- */
+
+mongoose.connection.close()
+
+/* Double-check with Marco */
